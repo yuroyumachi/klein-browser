e4dab88168a0a4d4a3bd299a0a28a94911197a9b
yuroyumachi <u6uma7@outlook.com>
2025-10-03 08:47:03 +0800

    moved class Document to common.py.

--- klein/htmllib.py
+++ klein/htmllib.py
@@ -1,40 +1,8 @@
 
 from __future__ import annotations
-from collections.abc import Iterator
 from enum import Enum
 from dataclasses import dataclass
-
-class Document:
-    def __init__(self, content: str) -> None:
-        self.content: Iterator[str] = iter(content)
-        self.buffer: list[str] = []
-
-    def getch(self)-> str:
-        if self.buffer != []:
-            return self.buffer.pop()
-
-        try:
-            return next(self.content)
-        except StopIteration:
-            return ""
-
-    def ungetch(self, ch: str)-> None:
-        self.buffer.append(ch)
-
-    def peek(self)-> str:
-        if self.buffer == []:
-            ch = self.getch()
-    
-            if ch != "":
-                self.ungetch(ch)
-    
-            return ch
-    
-        return self.buffer[-1]
-
-    def skip_whitespace(self)-> None:
-        while self.peek() and self.peek().isspace():
-            self.getch()
+from common import Document
 
 class HTMLTokenType(Enum):
     DATA = 0
